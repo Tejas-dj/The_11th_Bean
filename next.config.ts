c3e1c1015bb0cea40b19a580d026c610574a2788
Tejas-dj <tejas.dj@outlook.com>
2026-05-16 14:58:26 +0530

    caching

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,7 @@ const nextConfig: NextConfig = {
   compress: true,
   images: {
     formats: ['image/avif', 'image/webp'],
-    unoptimized: process.env.NODE_ENV === 'development',
-    // minimumCacheTTL: 2592000, // 30 days
+    minimumCacheTTL: 2592000, // 30 days
   },
 };
 
